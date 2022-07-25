a91ab5ff1746800fddda33db23ac2dfb0bc1f95b
Aamir Khan <105201200+webninja09@users.noreply.github.com>
2022-07-25 17:07:04 +0530

    api token salt added

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'NtmrV9HTrB8PoHH5kRJMcQ=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','DeiI+IUejWcorJpwMFOl9w=='),
   },
 });
